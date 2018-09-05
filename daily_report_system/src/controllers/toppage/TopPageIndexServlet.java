683ecf3ba0dd056d7dcc181a59df708921139d14
masahiro yasunaga <yasunaga37@gmail.com>
2018-09-05 12:54:45 +0900

    Modify top page

--- daily_report_system/src/controllers/toppage/TopPageIndexServlet.java
+++ daily_report_system/src/controllers/toppage/TopPageIndexServlet.java
@@ -1,7 +1,9 @@
 package controllers.toppage;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import models.Employee;
+import models.Report;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class TopPageIndexServlet
@@ -29,13 +35,41 @@ public class TopPageIndexServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // フラッシュ情報をsessionスコープからrequestスコープへ移動する
+
+		// ログイン中の従業員とEntityManagerの取得
 		HttpSession session = request.getSession();
+		Employee login_employee = (Employee) session.getAttribute("login_employee");
+		EntityManager em = DBUtil.createEntityManager();
+
+		// ページネーションの設定
+        int page;
+        try{
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(Exception e) {
+            page = 1;
+        }
+        List<Report> reports = em.createNamedQuery("getMyAllReports", Report.class)
+                                  .setParameter("employee", login_employee)
+                                  .setFirstResult(15 * (page - 1))
+                                  .setMaxResults(15)
+                                  .getResultList();
+        long reports_count = (long)em.createNamedQuery("getMyReportsCount", Long.class)
+                                     .setParameter("employee", login_employee)
+                                     .getSingleResult();
+        em.close();
+
+        // リクエスト情報の設定
+        request.setAttribute("reports", reports);
+        request.setAttribute("reports_count", reports_count);
+        request.setAttribute("page", page);
+
+		// フラッシュ情報をsessionスコープからrequestスコープへ移動する
 		String flush = (String)session.getAttribute("flush");
 		if (flush != null && !flush.equals("")) {
 			request.setAttribute("flush", flush);
 			session.removeAttribute("flush");
 		}
+
 		// /views/topPage/index.jspへディスパッチ
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
         rd.forward(request, response);
